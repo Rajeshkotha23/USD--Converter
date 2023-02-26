9e9ec8a3d2997ddcf85867df57aa74c9ae068a23
Rajeshrapo <rajeshkotha232@gmail.com>
2023-02-26 20:29:11 +0530

    Usd converter

---
+++ script.js
@@ -0,0 +1,10 @@
+function CurrencyConvert(){
+    var USD=document.getElementById('USD')
+ var value=USD.value
+ var result = value * 81.62
+ 
+ console.log(result)
+   var INR=document.getElementById('INR')
+     INR.value=result
+    var btn=document.getElementById
+}
